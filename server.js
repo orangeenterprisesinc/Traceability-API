30f1a6d0316b04cc33825080da0346a322da565f
AlexanderShumonov <sasha@orangesoftware.com>
2020-05-07 17:11:55 -0400

    server message changed

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+const server = app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
